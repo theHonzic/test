ccd59f936be6c510618ed094af3cbcca2304a95b
Jan Janovec <99410175+theHonzic@users.noreply.github.com>
2026-02-19 23:52:55 +0100

    chore: clean up main branch

--- minimal-package/Sources/MinimalPackage/MinimalPackage.swift
+++
@@ -1,6 +0,0 @@
-/// The MinimalPackage provides a core set of features for payment processing.
-///
-/// It consolidates features and core logic into a single cohesive library.
-///
-/// For more information, please see the ``MinimalPackageCore`` and ``MinimalPackageFeature`` modules.
-
